25d6cbcdcb554362ad041d6c1cc017e28bf6423c
kim nam ho <kimnamho95@gmail.com>
2026-08-18 08:59:24 +0800

    initial react n_library project

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,12 @@
+export function Welcome() {
+  return (
+    <main className="welcome">
+      <div className="welcome-content">
+        <h1>Welcome to React Router</h1>
+        <p>
+          Edit <code>app/routes/home.tsx</code> to get started.
+        </p>
+      </div>
+    </main>
+  );
+}
---
+++ read_me.txt
@@ -0,0 +1,8 @@
+node -v
+v22.19.0
+
+npm -v
+10.9.3
+
+react router v7
+8.3.0
